2a62f083c5fe955946e414988c93648e18d02d9f
omardoescode <omardoescode@gmail.com>
2024-10-01 15:33:15 +0300

    debug: add library interpositioning implementation for debugging `fread` and `fwrite`

---
+++ include/interpose.h
@@ -0,0 +1,16 @@
+#ifndef INTERPOSE_HEADER
+#define INTERPOSE_HEADER
+#include <dlfcn.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+size_t my_fread(void *__restrict __ptr, size_t __size, size_t __n,
+                FILE *__restrict __stream);
+
+size_t my_fwrite(const void *__restrict __ptr, size_t __size, size_t __n,
+                 FILE *__restrict __s);
+
+#define fread my_fread
+#define fwrite my_fwrite
+
+#endif
