58765c4828927ed62e7f35a03b87abf2ccd9c95e
omardoescode <omardoescode@gmail.com>
2024-10-03 19:35:02 +0300

    feat: main.c uses tree_serializer

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include "include/debug.h"
 #include "include/huffman_tree.h"
 #include "include/priority_queue.h"
+#include "include/tree_serializer.h"
 #include "include/vector.h"
 #include <assert.h>
 #include <limits.h>
@@ -9,12 +10,14 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <wchar.h>
 
-#define HEADER_END "\0"
 FILE *open_file(char *filename, char *mode);
 void compress(FILE *, FILE *);
-void write_header(vector *, FILE *);
+void uncompress(FILE *, FILE *);
+vector *read_header(FILE *);
+void write_header(huffman_node_t *, FILE *);
 void write_file(FILE *, FILE *, vector *);
 
 FILE *open_file(char *filename, char *mode) {
@@ -28,6 +31,11 @@ FILE *open_file(char *filename, char *mode) {
   return fp;
 }
 
+void write_header(huffman_node_t *tree, FILE *fp) {
+  tree_serializer *ts = ts_init(fp, tree);
+  ts_serialize(ts);
+  ts_dispose(ts);
+}
 // TODO: Fix when the input file is empty
 // It results in an error
 void compress(FILE *inp, FILE *out) {
@@ -60,38 +68,16 @@ void compress(FILE *inp, FILE *out) {
   }
 
   huffman_node_t *tree = hn_create_tree(queue);
+  /* hn_print(tree, 2); */
+  /* hn_assign_codes(tree, codes); */
 
-  hn_assign_codes(tree, codes);
-
-  write_header(codes, out);
-
-  rewind(inp);
-  write_file(inp, out, codes);
-
-  v_dispose(freq);
-  v_dispose(codes);
-}
-
-void write_header(vector *codes, FILE *out) {
-  bit_packer *bp = bp_init(out);
-
-  size_t length = 0;
-  for (int i = 0; i < v_size(codes); i++) {
-    if (*v_get(codes, i))
-      length++;
-  }
-
-  bp_pack(bp, length);
-
-  for (size_t i = 0; i < v_size(codes); i++) {
-    v_type val = *v_get(codes, i);
-    if (val)
-      bp_pack(bp, val);
-  }
-  putc('\n', out);
-
-  bp_flush(bp);
-  bp_dispose(bp);
+  write_header(tree, out);
+  /**/
+  /* rewind(inp); */
+  /* write_file(inp, out, codes); */
+  /**/
+  /* v_dispose(freq); */
+  /* v_dispose(codes); */
 }
 
 void write_file(FILE *inp, FILE *out, vector *codes) {
@@ -106,18 +92,38 @@ void write_file(FILE *inp, FILE *out, vector *codes) {
   bp_dispose(bp);
 }
 
+void uncompress(FILE *inp, FILE *out) { vector *codes = read_header(inp); }
+
+// Stack, queues, heap, hash table, sorting, tree.
+vector *read_header(FILE *in) {
+  tree_deserializer *ts = ts_init(in, NULL);
+  huffman_node_t *tree = ts_deserialize(ts);
+  hn_print(tree, 2);
+}
+
 int main(int argc, char *argv[argc]) {
   setlocale(LC_ALL, "");
   assert(sizeof(wchar_t) <=
          sizeof(size_t)); // Without this assertion, the program will crash
-  if (argc < 3) {
-    puts("Invalid Input; Usage: compress INPUT_FILE OUTPUT_FILE");
+  if (argc < 4) {
+    puts("Invalid Input; Usage: compress INPUT_FILE OPTION[-c|-u] "
+         "OUTPUT_FILE\nc: compress, u: uncompress");
     return 1;
   }
 
-  FILE *inp = open_file(argv[1], "r, ccs=UTF-8");
-  FILE *out = open_file(argv[2], "w, ccs=UTF-8");
-  compress(inp, out);
+  FILE *inp = open_file(argv[1], "rb, ccs=UTF-8");
+  FILE *out = open_file(argv[3], "w, ccs=UTF-8");
+  if (strcmp(argv[2], "-c") == 0)
+    compress(inp, out);
+  else if (strcmp(argv[2], "-u") == 0)
+    uncompress(inp, out);
+  else {
+    fclose(inp);
+    fclose(out);
+    puts("Invalid option");
+    return 1;
+  }
+
   fclose(inp);
   fclose(out);
   return EXIT_SUCCESS;
